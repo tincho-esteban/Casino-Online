f59d9e308860168c8aa34cf008754e81c426bd27
tincho-esteban <tinchoesteban2004@gmail.com>
2022-07-21 22:28:09 -0300

    agregar tostify

--- js/main.js
+++ js/main.js
@@ -1,51 +1,146 @@
+function btnIniciarSesion() {
+    let fondo = document.getElementById("fondo");
+    fondo.removeAttribute("class","invisible");
+}
+
+function ocultar() {
+    let fondo = document.getElementById("fondo");
+    fondo.setAttribute("class","invisible");
+}
+
+function mostrarInicioSesion() {
+    let sesion = document.getElementById('sesion');
+    let registro = document.getElementById('registro');
+    registro.setAttribute("class", "invisible")
+    sesion.removeAttribute("class", "invisible")
+}
+function mostrarRegistro() {
+    let sesion = document.getElementById('sesion');
+    let registro = document.getElementById('registro');
+    sesion.setAttribute("class", "invisible")
+    registro.removeAttribute("class", "invisible")
+}
+
 function registro() {
 
     let usuario = document.getElementById('usuario');
     let email = document.getElementById('email');
     let contraseña = document.getElementById('contraseña');
     let edad = document.getElementById('edad');
-    let error1 = document.getElementById('error1');
     let minusculas = /[a-z]/g;
     let mayusculas = /[A-Z]/g;
     let numeros = /[0-9]/g;
 
     if (usuario.value.length == 0) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "Introduce un nombre de usuario",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else if (email.value.length == 0) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "Introduce un mail",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else if (contraseña.value.length == 0) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "Introduce una contraseña",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else if (contraseña.value.length < 8) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "la contraseña debe tener al menos 8 caracteres",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else if (!contraseña.value.match(minusculas)) {
-        error1.innerHTML += "error";
+        Toastify({
+        text: "la contraseña debe tener al una minúscula",
+        duration: 3000,
+        style: {
+            background: "red"
+        }
+        }).showToast();
     } else if (!contraseña.value.match(mayusculas)) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "la contraseña debe tener al menos una mayúscula",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else if (!contraseña.value.match(numeros)) {
-        error1.innerHTML += "error";
+        Toastify({
+            text: "la contraseña debe tener al menos un numero",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     } else {
         localStorage.setItem('usuario', usuario.value);
         localStorage.setItem('email', email.value);
         localStorage.setItem('contraseña', contraseña.value);
         localStorage.setItem('edad', edad.value);
-        error1.innerHTML += "registro completo";
-        error1.setAttribute("class", "verde");
+        Toastify({
+            text: "registro completo",
+            duration: 3000,
+            style: {
+                background: "green"
+            }
+            }).showToast();
+            let fondo = document.getElementById("fondo");
+            fondo.setAttribute("class","invisible");
     }
+
+return(false)
 }
 
 
 function inicioSesion() {
     let nombreGuardado = localStorage.getItem('usuario');
-    let emailGuardado = localStorage.getItem('email');
     let contraseñaGuardada = localStorage.getItem('contraseña');
 
     let nombreUsuario = document.getElementById('nombreUsuario');
     let contraseñaUsuario = document.getElementById('contraseñaUsuario');
 
     if (nombreUsuario.value == nombreGuardado && contraseñaUsuario.value == contraseñaGuardada) {
-        error2.innerHTML += "Inicio de sesión correcto"
-        error2.setAttribute("class", "verde");
-    } else {
-        error2.innerHTML += "error"
+        Toastify({
+            text: "Inicio de sesión correcto",
+            duration: 3000,
+            style: {
+                background: "green"
+            }
+            }).showToast();
+            let fondo = document.getElementById("fondo");
+            fondo.setAttribute("class","invisible");
+    } else if (nombreUsuario.value != nombreGuardado){
+        Toastify({
+            text: "Usuario incorrecto",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
+    } else if (contraseñaUsuario.value != contraseñaGuardada){
+        Toastify({
+            text: "Contraseña incorrecta",
+            duration: 3000,
+            style: {
+                background: "red"
+            }
+            }).showToast();
     }
+    return(false)
 }
+
