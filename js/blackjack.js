e3070414dffeaff196bb0b68ed9edc89cc81806e
tincho-esteban <tinchoesteban2004@gmail.com>
2022-08-18 20:12:26 -0300

    arreglos blackjack.js
    simplificación del código, junte dos funciones que prácticamente hacían lo mismo
    dos valores al As, ahora puede valer 11 o 1 como corresponde

--- js/blackjack.js
+++ js/blackjack.js
@@ -9,10 +9,11 @@ class carta {
     static x = 50;
     static y = 50;
 
-    constructor(id, valor, palo) {
+    constructor(id, valor, valor2, palo) {
         this.img = new Image();
         this.id = id;
         this.valor = valor;
+        this.valor2 = valor2;
         this.palo = palo;
     }
 }
@@ -23,14 +24,15 @@ let cartasCrupier = [];
 let indiceCarta = 0;
 let palos = ["S", "H", "D", "C"];
 for (i = 0; i < 4; i++) {
-    for (j = 1; j <= 10; j++) {
-        cartas.push(new carta(j, j, palos[i]));
+    cartas.push(new carta(1, 11, 1, palos[i]));
+    for (j = 2; j <= 10; j++) {
+        cartas.push(new carta(j, j, j, palos[i]));
     }
     for(j = 11; j <= 13; j++) {
-        cartas.push(new carta(j, 10, palos[i]));
+        cartas.push(new carta(j, 10, 10, palos[i]));
     }
 }
-for (i = 0; i < 200; i++) {
+for (i = 0; i < 500; i++) {
     cartas.splice(Math.random() * 52, 0, cartas[0]);
     cartas.shift();
 }
@@ -51,68 +53,75 @@ function pedirCarta() {
             dibujarCarta(CJ);
             indiceCarta++;
         }
-        mostrarPuntos()
+        check21()
     } else if (indiceCarta < 8) {
         let CJ = cartas[indiceCarta];
         cartasJugador.push(CJ);
         dibujarCarta(CJ);
         indiceCarta++;
-        mostrarPuntos()
         check21()
     }
 }
 
 function check21(){
     let puntosUsuario = 0;
+    let puntosUsuario2 = 0;
     let info = document.getElementById("info");
         for (i in cartasJugador) {
-        puntosUsuario += cartasJugador[i].valor;
+            puntosUsuario += cartasJugador[i].valor;
+            puntosUsuario2 += cartasJugador[i].valor2;
         }
-    if (puntosUsuario > 21) {
-        document.getElementById("pedir").disabled = true;
-        document.getElementById("plantar").disabled = true;
-        document.getElementById("reset").style.visibility = "visible";
-        mostrarPuntos()
-        info.innerHTML +="<br><b>Te pasaste de 21</b>";
-    }
+        info.innerHTML = "Tu puntuación: " + puntosUsuario;
+        if (puntosUsuario > 21) {
+            info.innerHTML = "Tu puntuación: " + puntosUsuario2;
+        }
+        if (puntosUsuario > 21 && puntosUsuario2 > 21) {
+            document.getElementById("pedir").disabled = true;
+            document.getElementById("plantar").disabled = true;
+            document.getElementById("reset").style.visibility = "visible";
+            info.innerHTML +="<br><b>Te pasaste de 21</b>";
+        }
 }
 
-function mostrarPuntos() {
-    let puntosUsuario = 0;
-    for (i in cartasJugador) {
-        puntosUsuario += cartasJugador[i].valor;
-    }
-    info.innerHTML = "Puntuación jugador: " + puntosUsuario;
-}
+// function mostrarPuntos() {
+//     let puntosUsuario = 0;
+//     for (i in cartasJugador) {
+//         puntosUsuario += cartasJugador[i].valor;
+//     }
+//     info.innerHTML = "Tu puntuación: " + puntosUsuario;
+// }
 
 function plantarme() {
     document.getElementById("pedir").disabled = true;
     document.getElementById("plantar").disabled = true;
     document.getElementById("reset").style.visibility = "visible";
     let puntosUsuario = 0;
+    let puntosUsuario2 = 0;
     let puntosCrupier = 0;
     let info = document.getElementById("info");
     for (i in cartasJugador) {
         puntosUsuario += cartasJugador[i].valor;
+        puntosUsuario2 += cartasJugador[i].valor2;
     }
     while (puntosCrupier < 17) {
         cartasCrupier.push(cartas[indiceCarta]);
         puntosCrupier += cartas[indiceCarta].valor;
         indiceCarta++;
     }
-    info.innerHTML = "Puntuación jugador: " + puntosUsuario + "<br>Puntuación crupier: " + puntosCrupier;
+    info.innerHTML = "Tu puntuación: " + puntosUsuario + "<br>Puntuación crupier: " + puntosCrupier;
+    if (puntosUsuario > 21) {
+        info.innerHTML = "Tu puntuación: " + puntosUsuario2 + "<br>Puntuación crupier: " + puntosCrupier;
+    }
     carta.x = 50;
     carta.y = 400;
     for (i in cartasCrupier) {
         dibujarCarta(cartasCrupier[i]);
     }
-    if (puntosUsuario == 21) {
-        info.innerHTML +="<br><b>Blackjack!!! Ganaste!</b>";
-    } else if (puntosUsuario > 21) {
-        info.innerHTML +="<br><b>Te pasaste de puntos</b>";
+    if (puntosUsuario > 21 && puntosUsuario2 > 21) {
+        info.innerHTML +="<br><b>Te pasaste de 21</b>";
     } else if (puntosCrupier > 21) {
-        info.innerHTML +="<br><b>Ganaste!!! El croupier se paso de puntos</b>";
-    } else if (puntosCrupier >= puntosUsuario) {
+        info.innerHTML +="<br><b>Ganaste!!! El croupier se paso de 21</b>";
+    } else if (puntosCrupier >= puntosUsuario && puntosCrupier >= puntosUsuario2) {
         info.innerHTML += "<br><b>Gano el croupier...</b>";
     } else {
         info.innerHTML += "<br><b>Ganaste!!!</b>";
